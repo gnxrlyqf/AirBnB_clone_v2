5a21c7ee9f9dac2590f552a6fa7e3174a71a67eb
gnxrlyqf <gnarlydestructor@outlook.com>
2023-12-17 18:56:24 +0100

    sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
---database setum for hbnb
+--database setup for hbnb
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY hbnb_dev_pwd;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
-GRANT SELECT ON performace_schema TO hbnb_dev@localhost;
+GRANT SELECT ON performace_schema.* TO hbnb_dev@localhost;
 FLUSH PRIVILEGES
