dafffe4c9a77c2ef1424460345aabd946188b3b1
gnxrlyqf <gnarlydestructor@outlook.com>
2023-12-17 17:53:28 +0100

    console

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+import re
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,16 +20,49 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
     classes = {
-               'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City, 'Amenity': Amenity,
-               'Review': Review
-              }
+        'BaseModel': BaseModel,
+        'User': User,
+        'Place': Place,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Review': Review
+    }
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
-             'number_rooms': int, 'number_bathrooms': int,
-             'max_guest': int, 'price_by_night': int,
-             'latitude': float, 'longitude': float
-            }
+        'name': str,
+        'city_id': str,
+        'user_id': str,
+        'number_rooms': int,
+        'number_bathrooms': int,
+        'max_guest': int,
+        'price_by_night': int,
+        'latitude': float,
+        'longitude': float
+    }
+
+    def strtok(self, string):
+        result = re.split(r'\s+', string)
+        output = {}
+        for token in result:
+            current = re.search(r'.+?(?==)', token)
+            if not current or current.group(0) not in self.types:
+                continue
+            key = current.group(0)
+            if self.types[key] == str:
+                output[key] = re.search(r'(?<=")(?:\\.|[^"\\])*(?=")',
+                                        token).group(0)
+            elif self.types[key] == int:
+                value = re.search(r'(?<==).+', token).group(0)
+                n = re.search(r'^[+-]?\d+$', value)
+                if n:
+                    output[key] = n.group(0)
+            elif self.types[key] == float:
+                value = re.search(r'(?<==).+', token).group(0)
+                n = re.search(r'^[+-]?([0-9]*[.])?[0-9]+$', value)
+                if n:
+                    output[key] = n.group(0)
+        return output
 
     def preloop(self):
         """Prints if isatty is false"""
@@ -73,7 +107,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,13 +149,14 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        name = re.search(r'^\S+', args).group(0)
+        if not name:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[name](self.strtok(args))
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -319,6 +354,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
