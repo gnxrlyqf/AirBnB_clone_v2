a3865df39cbb716ab3b48079934a71b17c0d129f
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-13 13:27:28 +0100

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+#setup server for deploy static
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "Hello, world!" > /data/web_static/releases/test/index.html
+echo "HBNB Static" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 conf="51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}"
