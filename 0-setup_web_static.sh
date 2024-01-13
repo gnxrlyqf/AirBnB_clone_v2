22e234385aeb380882b4426bf9eaae23e1fccd27
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-13 14:01:45 +0100

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,5 @@ mkdir -p /data/web_static/releases/test/
 echo "HBNB Static" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
-conf="51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}"
-sed -i "$conf" /etc/nginx/sites-available/default
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
