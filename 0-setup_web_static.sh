7dd512e35865a1a3c791c79c079bee51952dc96e
gnxrlyqf <gnarlydestructor@outlook.com>
2024-01-13 14:06:01 +0100

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,13 @@ sudo apt update
 sudo apt install nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "HBNB Static" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
